4bc6884814ef05d95baa3306b10830384f14f7a1
Tiara Brunt <53661450+tiara143@users.noreply.github.com>
2021-10-22 18:23:24 -0500

    10/22/2021
    Placed a template string inside a function redering the list.

--- Google Chrome Extension/dist/app.js
+++ Google Chrome Extension/dist/app.js
@@ -7,8 +7,31 @@ let mySaves = []
 inputBtn.addEventListener("click", function () {
     // console.log("Button clicked from addEventListener")
     mySaves.push(inputEl.value)
-    ulEl.textContent = mySaves
-    console.log(mySaves)
+    // ulEl.textContent = mySaves
+    inputEl.value = ""
+    renderSaves()
 })
+
+
+function renderSaves() {
+    let listItems = ""
+    for (let i = 0; i < mySaves.length; i++) {
+        //The += allows the loop to take the previous value and concatenate it with the current array the user is looking at
+        // ulEl.innerHTML += "<li>" + mySaves[i] + "</li>"
+        listItems += `
+    <li>
+        <a target='_blank' href='${mySaves[i]}'>
+            ${mySaves[i]}
+        </a>
+    </li>
+    `
+    }
+    ulEl.innerHTML = listItems
+}
+
+
+
+
+
 
 

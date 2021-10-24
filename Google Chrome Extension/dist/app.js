fbcb995864d11a95ba036884adb0619386af9a73
Tiara Brunt <53661450+tiara143@users.noreply.github.com>
2021-10-24 15:29:46 -0500

    10/24/2021
    Fixed some bugging and technical issues.

--- Google Chrome Extension/dist/app.js
+++ Google Chrome Extension/dist/app.js
@@ -6,42 +6,42 @@ const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("mySaves"))
 // console.log(leadsFromLocalStorage)
 
-deleteBtn.addEventListener("dblclick", function () {
-    localStorage.clear()
-    mySaves = []
-    renderSaves()
-})
-
 // //truthy value statement being tested
-// if (leadsFromLocaStorage) {
-//     mySaves = leadsFromLocalStorage
-//     renderSaves()
-// }
+if (leadsFromLocalStorage) {
+    mySaves = leadsFromLocalStorage
+    render(mySaves)
+}
 
-inputBtn.addEventListener("click", function () {
-    mySaves.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("mySaves", JSON.stringify(mySaves))
-    renderSaves()
-    //to verify it works
-    // console.log(localStorage.getItem("mySaves"))
-})
-
-function renderSaves() {
+function render(saves) {
     let listItems = ""
-    for (let i = 0; i < mySaves.length; i++) {
+    for (let i = 0; i < saves.length; i++) {
         //The += allows the loop to take the previous value and concatenate it with the current array the user is looking at
         // ulEl.innerHTML += "<li>" + mySaves[i] + "</li>"
         listItems += `
     <li>
-        <a target='_blank' href='${mySaves[i]}'>
-            ${mySaves[i]}
+        <a target='_blank' href='${saves[i]}'>
+            ${saves[i]}
         </a>
     </li>
     `
     }
     ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    mySaves = []
+    render(mySaves)
+})
+
+inputBtn.addEventListener("click", function () {
+    mySaves.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("mySaves", JSON.stringify(mySaves))
+    render(mySaves)
+    //to verify it works
+    // console.log(localStorage.getItem("mySaves"))
+})
 
 
 

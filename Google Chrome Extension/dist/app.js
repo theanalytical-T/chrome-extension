477684a426d07bf73f5286090d07085c0ee4a707
Tiara Brunt <53661450+tiara143@users.noreply.github.com>
2021-10-25 21:05:45 -0500

    10/25/2021
    Overall finished with the chrome extension, deploying on chrome while using a firefox browser for viewing might play a part in problems with deploying on the chrome network. Will check into that soon.

--- Google Chrome Extension/dist/app.js
+++ Google Chrome Extension/dist/app.js
@@ -4,7 +4,13 @@ const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("mySaves"))
-// console.log(leadsFromLocalStorage)
+const tabBtn = document.getElementById("tab-btn")
+//an object inside an array, get comfortable with these!
+const tabs = [
+    { url: "https://www.linkedin.com/in/tiara-brunt-7ab01518a/" }
+]
+
+// conssdfole.log(leadsFromLocalStorage)
 
 // //truthy value statement being tested
 if (leadsFromLocalStorage) {
@@ -27,6 +33,18 @@ function render(saves) {
     }
     ulEl.innerHTML = listItems
 }
+
+tabBtn.addEventListener("click", function () {
+    // grabs the URL of the current tab!
+    //since only one tab should be active and in the current window at once
+    //the return variable should only have one entry
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        mySaves.push(tabs[0].url)
+        localStorage.setItem("mySaves", JSON.stringify(mySaves))
+        inputEl.value = ""
+        render(mySaves)
+    })
+})
 
 deleteBtn.addEventListener("dblclick", function () {
     localStorage.clear()
